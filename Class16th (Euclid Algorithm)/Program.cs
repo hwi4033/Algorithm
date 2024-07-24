26c017be6e14d4adbd1637870592b9b048fedce1
hwi4033 <ckdgnl6381@naver.com>
2024-07-24 15:45:56 +0900

    2024.07.24 백트레킹

---
+++ Class16th (Euclid Algorithm)/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Class16th__Euclid_Algorithm_
+{
+    internal class Program
+    {
+        static int Euclid(int x, int y)
+        {
+            // 유클리드 호제법
+            if (y == 0)
+            {
+                return x;
+            }
+            else
+            {
+                return Euclid(y, x % y);
+            }
+        }
+
+        static int Eu(int x, int y)
+        {
+            int max = Math.Max(x, y);
+            int min = Math.Min(x, y);
+            int temp = max % min;
+
+            if (temp == 0)
+            {
+                return min;
+            }
+            else
+            {
+                return Eu(min, temp);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            #region 유클리드 호제법
+            // 2개의 자연수 또는 정식의 최대 공약수를 구하는 알고리즘으로 
+            // 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘이다.
+            // O(log n)?
+
+            int x = 30, y = 48;
+            int temp = 1;
+
+            for (int i = 2; i <= x;)
+            {
+                if (x % i == 0 && y % i == 0)
+                {
+                    x = x / i;
+                    y = y / i;
+
+                    temp *= i;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            // 일반 풀이
+            int result = 0;
+
+            for (int i = 1; i <= Math.Min(x, y); i++)
+            {
+                if (x % i == 0 && y % i == 0)
+                {
+                    result = i;
+                }
+            }
+
+            Console.WriteLine("x : " + x);
+            Console.WriteLine("y : " + y);
+            Console.WriteLine("최대 공약수 : " + temp);
+
+            // 두 수 중에서, 큰 수(A)를 작은 수(B)로 나눈 다음
+            // 작은 수(B)를 큰 수를 작은 수로 나눈 나머지(C)로 나눠서 나머지가 0일 때까지 반복한다.
+
+            Console.WriteLine(Euclid(78696, 19332));
+            #endregion
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -4,11 +4,57 @@ using System.Runtime.ExceptionServices;
 
 namespace Program
 {
+
     internal class Program
     {
+        static void hanoi(int n, char from, char by, char to)
+        {
+            if (n < 0)
+            {
+                Console.WriteLine(from + " -> " + to);
+                return;
+            }
+
+            Console.WriteLine(from + " -> " + to);
+            hanoi(n - 1, 'A', 'C', 'B');
+            Console.WriteLine(from + " -> " + to);
+        }
+
         static void Main(string[] args)
         {
-            
+            #region 백트레킹
+            // 해를 찾아가는 도중에 지금의 경로가 해가 될 것 같지 않으면,
+            // 더 이상 깊이 들어가지 않고, 이전 단계로 다시 돌아가는 알고리즘이다.
+
+            // 백트레킹의 유망성 판단
+            // 해가 될 만한지 판단한 후에 유망하지 않다고 결정되면,
+            // 그 노드의 이전 노드로 돌아가 다음 자식 노드로 이동한다.
+
+            // 해가 될 만한 가능성이 있으면 유망하다 - (Promising)
+            // 유망하지 않은 노드에 가지 않는 것 - (Pruning)
+
+            // 하노이
+            // 1. 한 번에 하나의 원판만 이동한다.
+            // 2. 맨 위에 있는 원판만 이동 가능하다.
+            // 3. 크기가 작은 원판 위에 큰 원판을 쌓을 수 없다.
+
+            hanoi(3, 'A', 'B', 'C');
+            // a c
+            // A C B
+            // a b
+            // c b
+
+            // a c
+            // A C B
+            // b a
+            // b c
+
+            // a c
+            // from to
+            // from by
+            // to by
+            // from to
+            #endregion
         }
     }
 }
